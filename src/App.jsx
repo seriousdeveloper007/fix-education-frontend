bdaccd4c2bcb1dc3bd395c15e2745c8e3a35b8a6
Arpit Agrawal <58815528+arpitdhncve@users.noreply.github.com>
2025-09-29 09:04:43 +0530

    Add classroom route with blank page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage.jsx';
+import ClassroomPage from './pages/ClassroomPage.jsx';
 import VerifyLearner from './components/VerifyLearner.jsx';
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/classroom" element={<ClassroomPage />} />
         <Route path="/verify-learner" element={<VerifyLearner />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
